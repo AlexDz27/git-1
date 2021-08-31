4a9bdebb167f9b3bc794bbdcf5f825e476080a8d
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-08-31 22:18:59 +0300

    Write JS code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-const some = 'some'
+const featureOneElement = document.querySelector('.feature-1');
 
-console.log(some)
+featureOneElement.innerHTML = 'My feature 1!';
+
+// another new line
+
+// another new line 2
+
+// another new line
+
+// another new line 4
+
+// another new line 5
+
+// another new line 6
+
+// another new line 7
+
+// another new line 8
+
+// another new line 9
